b7b168713ddc4af98ee8960629cf91449447786e
ktv <32715614+Solvtions@users.noreply.github.com>
2019-03-11 12:44:28 -0400

    wizard update

--- plugin.program.ktvwizard/uservar.py
+++ plugin.program.ktvwizard/uservar.py
@@ -1,96 +1,35 @@
 import os, xbmc, xbmcaddon
-import binascii
+
 #########################################################
 ### User Edit Variables #################################
 #########################################################
-# Enable/Disable the text file caching with 'Yes' or 'No' and age being how often it rechecks in minutes
-CACHETEXT      = 'Yes'
-CACHEAGE       = 30
-
 ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
 ADDONTITLE     = '[COLOR red][B]Kritik TV Wizard[/B][/COLOR]'
-BUILDERNAME    = 'Kritik'
-#########################Make sure to change the repo to yours!!!!
-EXCLUDES       = [ADDON_ID, 'repository.ktvrepo', 'roms', 'My_Builds', 'backupdir']
-BUILDFILE      = 'http://kritiktv.net/ktv/Texts/builds.txt'
+EXCLUDES       = [ADDON_ID, 'repository.ktvrepo']
+# Text File with build info in it.
+BUILDFILE      = 'http://kritiktv.net/ktv/Texts/autobuilds.txt'
+# How often you would list it to check for build updates in days
+# 0 being every startup of kodi
 UPDATECHECK    = 0
-APKFILE        = 'http://kritiktv.net/ktv/Texts/apks.txt'
-YOUTUBETITLE   = 'Kritik TV' 
+# Text File with apk info in it.  Leave as 'http://' to ignore
+APKFILE        = 'http://'
+# Text File with Youtube Videos urls.  Leave as 'http://' to ignore
+YOUTUBETITLE   = 'YouTube Help Videos'
 YOUTUBEFILE    = 'http://'
+# Text File for addon installer.  Leave as 'http://' to ignore
 ADDONFILE      = 'http://'
+# Text File for advanced settings.  Leave as 'http://' to ignore
 ADVANCEDFILE   = 'http://'
+# Text file for roms and emus
 ROMPACK        = 'http://'
 EMUAPKS        = 'http://'
-ADDONPACK      = 'http://'
+
+# Dont need to edit just here for icons stored locally
 PATH           = xbmcaddon.Addon().getAddonInfo('path')
 ART            = os.path.join(PATH, 'resources', 'art')
 
 #########################################################
 ### THEMING MENU ITEMS ##################################
-#########################################################
-
-##Alway test to see the color combo!!
-
-#### NEW GUI THEME ###################################
-# Choose from the following 
-# Only these colors avalable
-# white , blue , orange , yellow , red , purple , pink , lime , cyan, green
-#Button focus color
-FOCUS_BUTTON_COLOR = 'red'
-EXIT_BUTTON_COLOR = 'white'
-#Highlight outline for lists
-HIGHLIGHT_LIST = 'orange'
-##No TXT file Banner
-NO_TXT_FILE = 'red'
-
-############################################
-############################################
-### The full list of colors for below can found @ https://forum.kodi.tv/showthread.php?tid=210837
-
-#Top Main buttons
-MAIN_BUTTONS_TEXT = 'white'
-#All other buttons
-OTHER_BUTTONS_TEXT = 'orange'
-#all list text color
-##FYI any color placed in the txt file will overide this
-LIST_TEXT = 'redorange'
-
-
-#Description text title color
-DES_T_COLOR = 'red'
-#Description color
-DESCOLOR = 'white'
-
-#Wizard title name and verion color
-WIZTITLE = 'Kritik TV Wizard'
-WIZTITLE_COLOR = 'red'
-VERTITLE_COLOR = 'white'
-VER_NUMBER_COLOR = 'red'
-############################################################
-
-## The colors and theme below is still used for the pop up dialogs
-##Alway test to see the color combo
-# You can edit these however you want, just make sure that you have a %s in each of the
-# THEME's so it grabs the text from the menu item
-COLOR1         = 'red'
-COLOR2         = 'white'
-COLOR3         = 'orange'
-COLOR4         = 'snow'
-COLOR5         = 'redorange'
-# Primary menu items   / %s is the menu item and is required
-THEME1         = '[COLOR '+COLOR1+']%s[/COLOR]'
-# Build Names          / %s is the menu item and is required
-THEME2         = '[COLOR '+COLOR1+']%s[/COLOR]'
-# Alternate items      / %s is the menu item and is required
-THEME3         = '[COLOR '+COLOR2+']%s[/COLOR]'
-# Current Build Header / %s is the menu item and is required
-THEME4         = '[COLOR '+COLOR2+']Current Build:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
-# Current Theme Header / %s is the menu item and is required
-THEME5         = '[COLOR '+COLOR2+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
-THEME6         = '[COLOR '+COLOR3+'][B]%s[/B][/COLOR]'
-
-
-
 #########################################################
 # If you want to use locally stored icons the place them in the Resources/Art/
 # folder of the wizard then use os.path.join(ART, 'imagename.png')
@@ -98,29 +37,46 @@ THEME6         = '[COLOR '+COLOR3+'][B]%s[/B][/COLOR]'
 # Example:  ICONMAINT     = os.path.join(ART, 'mainticon.png')
 #           ICONSETTINGS  = 'http://aftermathwizard.net/repo/wizard/settings.png'
 # Leave as http:// for default icon
-ICONBUILDS     = 'http://'
-ICONMAINT      = 'http://'
-ICONAPK        = 'http://'
-ICONADDONS     = 'http://'
-ICONYOUTUBE    = 'http://'
-ICONSAVE       = 'http://'
-ICONTRAKT      = 'http://'
-ICONREAL       = 'http://'
-ICONLOGIN      = 'http://'
-ICONCONTACT    = 'http://'
-ICONSETTINGS   = 'http://'
+ICONBUILDS     = 'http://kritiktv.net/ktv/Images/builds.png'
+ICONMAINT      = 'http://kritiktv.net/ktv/Images/maintain.png'
+ICONSPEED      = 'http://kritiktv.net/ktv/Images/speedtest.png'
+ICONAPK        = 'http://kritiktv.net/ktv/Images/apk.png'
+ICONRETRO      = 'http://kritiktv.net/ktv/Images/retro.png'
+ICONADDONS     = 'http://kritiktv.net/ktv/Images/addon.png'
+ICONYOUTUBE    = 'http://kritiktv.net/ktv/Images/youtube.jpg'
+ICONSAVE       = 'http://kritiktv.net/ktv/Images/save.jpg'
+ICONTRAKT      = 'http://kritiktv.net/ktv/Images/trakt.jpg'
+ICONREAL       = 'http://kritiktv.net/ktv/Images/debrid.jpg'
+ICONLOGIN      = 'http://kritiktv.net/ktv/Images/login.jpg'
+ICONCONTACT    = 'http://kritiktv.net/ktv/Images/contact.jpg'
+ICONSETTINGS   = 'http://kritiktv.net/ktv/Images/settings.jpg'
 # Hide the ====== seperators 'Yes' or 'No'
 HIDESPACERS    = 'No'
 # Character used in seperator
-SPACER         = '-'
+SPACER         = '*'
+
+# You can edit these however you want, just make sure that you have a %s in each of the
+# THEME's so it grabs the text from the menu item
+COLOR1         = 'red'
+COLOR2         = 'white'
+# Primary menu items   / %s is the menu item and is required
+THEME1         = '[COLOR '+COLOR1+'][B]([COLOR '+COLOR2+']Kritik TV Wizard[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+# Build Names          / %s is the menu item and is required
+THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'
+# Alternate items      / %s is the menu item and is required
+THEME3         = '[COLOR '+COLOR1+']%s[/COLOR]'
+# Current Build Header / %s is the menu item and is required
+THEME4         = '[COLOR '+COLOR1+'][B]Current Build:[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+# Current Theme Header / %s is the menu item and is required
+THEME5         = '[COLOR '+COLOR1+'][B]Current Theme:[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
 
 # Message for Contact Page
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT    = 'No'
 # You can add \n to do line breaks
-CONTACT        = 'Thank you for choosing KriTiK TV Wizard.\r\n\r\nContact us at:  support@kritiktv.net'
+CONTACT        = 'Thank you for choosing KriTiK TV Wizard.\r\n\r\n Contact us at:  support@kritiktv.net'
 #Images used for the contact window.  http:// for default icon and fanart
-CONTACTICON    = os.path.join(ART, 'icon.png')
+CONTACTICON    = 'http://kritiktv.net/ktv/Images/contact.png'
 CONTACTFANART  = 'http://kritiktv.net/repo2/Wallpaper/System/ktvsystem.jpg'
 #########################################################
 
@@ -130,7 +86,7 @@ CONTACTFANART  = 'http://kritiktv.net/repo2/Wallpaper/System/ktvsystem.jpg'
 # Enable Auto Update 'Yes' or 'No'
 AUTOUPDATE     = 'Yes'
 # Url to wizard version
-WIZARDFILE     = 'http://kritiktv.net/ktv/Texts/builds.txt'
+WIZARDFILE     = 'http://kritiktv.net/ktv/Texts/autobuilds.txt'
 #########################################################
 
 #########################################################
@@ -139,11 +95,11 @@ WIZARDFILE     = 'http://kritiktv.net/ktv/Texts/builds.txt'
 # Enable Auto Install 'Yes' or 'No'
 AUTOINSTALL    = 'No'
 # Addon ID for the repository
-REPOID         = 'repository.ktvrepo'
+REPOID         = ''
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML   = 'https://raw.githubusercontent.com/Solvtions/ktvrepo/master/repository.ktvrepo/addons.xml'
+REPOADDONXML   = 'http://'
 # Url to folder zip is located in
-REPOZIPURL     =  'http://kritiktv.net/ktv/repo'
+REPOZIPURL     = 'http://'
 #########################################################
 
 #########################################################
@@ -155,13 +111,9 @@ ENABLE         = 'Yes'
 NOTIFICATION   = 'http://kritiktv.net/ktv/Texts/notify.txt'
 # Use either 'Text' or 'Image'
 HEADERTYPE     = 'Image'
-# Font size of header
-FONTHEADER     = 'Font13'
 HEADERMESSAGE  = 'Kritik TV Wizard'
-# url to image if using Image 424x180
+# url to image if using Image 500x50(Width can vary but height of image needs to be 50px)
 HEADERIMAGE    = ''
-# Font for Notification Window
-FONTSETTINGS   = 'Font12'
 # Background for Notification Window
 BACKGROUND     = 'http://kritiktv.net/ktv/Images/notify.jpg'
-############################    #############################
+#########################################################
